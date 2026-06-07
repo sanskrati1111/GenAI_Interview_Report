d5b78eccf3f0cb0a12302d52cb74b89c3799771f
sanskrati1111 <sanskrati123arya@gmail.com>
2026-06-07 23:26:03 +0530

    adding urls

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://genai-interview-report.onrender.com",
     withCredentials: true,
 })
 
